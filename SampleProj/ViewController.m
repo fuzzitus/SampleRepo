d6d04c66f715506fcbdc6ef4c4ee597a5dae023d
Akinbiyi Adesina Lalude <Adesina@J-Camille-Owens-Lalude-Computer.local>
2013-03-18 18:56:37 -0400

    Approval for release

--- SampleProj/ViewController.m
+++ SampleProj/ViewController.m
@@ -12,6 +12,13 @@
 
 //Edited by knathan
 
+//Edited by knathan
+// * Bullet 1 - okay
+// * Bullet 2 - needs work
+// * Bullet 3 - finished
+
+// All bullets addressed, ready for merger into master
+
 #import "ViewController.h"
 
 @interface ViewController ()
